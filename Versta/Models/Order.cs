48e656a865fb5902595cf997a00bc98e19a71223
Oleg Esikov <esikov.oleg@mail.ru>
2021-08-17 14:49:38 +0300

    Добавлены атрибуты валидации и аннотации данных

--- Versta/Models/Order.cs
+++ Versta/Models/Order.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Versta.Models
 {
     public class Order
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Не указан город отправителя")]
+        [Display(Name = "Город отправителя")]
         public string SenderCity { get; set; }
+
+        [Required(ErrorMessage = "Не указан адрес отправителя")]
+        [Display(Name = "Адрес отправителя")]
         public string SenderAddress { get; set; }
+
+        [Required(ErrorMessage = "Не указан город получателя")]
+        [Display(Name = "Город получателя")]
         public string RecipientCity { get; set; }
+
+        [Required(ErrorMessage = "Не указан адрес получателя")]
+        [Display(Name = "Адрес получателя")]
         public string RecipientAddress { get; set; }
+
+        [Required(ErrorMessage = "Не указан вес груза")]
+        [Display(Name = "Вес груза в кг")]
+        [Range(0.001, double.MaxValue, ErrorMessage = "Вес груза не может быть отрицательным или нулевым")]
         public double WeightInKg { get; set; }
+
+        [Required(ErrorMessage = "Не указана дата забора груза")]
+        [Display(Name = "Дата забора груза")]
+        [DataType(DataType.Date)]
         public DateTime DateOfReceiving { get; set; }
     }
 }
