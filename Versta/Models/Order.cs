6f9cc43763102cb545512f126f5c0f8819b33e29
Oleg Esikov <esikov.oleg@mail.ru>
2021-08-17 11:43:26 +0300

    Добавлена модель заказа

---
+++ Versta/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Versta.Models
+{
+    public class Order
+    {
+        public string SenderCity { get; set; }
+        public string SenderAddress { get; set; }
+        public string RecipientCity { get; set; }
+        public string RecipientAddress { get; set; }
+        public double Weight { get; set; }
+        public DateTime DateOfReceiving { get; set; }
+    }
+}
